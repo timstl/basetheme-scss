1a39a94a21ecae7810c29d12690bbff08fd4d7b0
Tim Gieseking <timstl@gmail.com>
2016-02-05 11:05:35 -0600

    update handling of mobile menus so resizing in/out works. update use of SSM to calculate sizes from variables.

--- js/main.js
+++ js/main.js
@@ -1,102 +1,108 @@
+/* adjust sizes to match _settings.scss */
+var _ssm_settings = { 
+	tablet_min: 768,
+	desktop_min: 960
+};
+
 (function(){
-    ssm.addState({
-        id: 'mobile',
-        maxWidth: 767,
-        onEnter: function()
-        { 
-        	runMobile(); 
-        }
-    });
-
-    ssm.addState({
-        id: 'tablet',
-        minWidth: 768,
-        maxWidth: 959,
-        onEnter: function() 
-        { 
-        	runTablet();
-        	runTabletOrBigger(); 
-        }
-    });
-
-    ssm.addState({
-        id: 'desktop',
-        minWidth: 960,
-        onEnter: function() 
-        { 
+	ssm.addState({
+		id: 'mobile',
+		maxWidth: parseInt(_ssm_settings.tablet_min) - 1,
+		onEnter: function()
+		{ 
+			initMobileMenu();
+			runMobile(); 
+		}
+	});
+	
+	ssm.addState({
+		id: 'tablet',
+		minWidth: parseInt(_ssm_settings.tablet_min),
+		maxWidth: parseInt(_ssm_settings.desktop_min) - 1,
+		onEnter: function() 
+		{ 
+			initMobileMenu();
+			runTablet();
+			runTabletOrBigger(); 
+		}
+	});
+	
+	ssm.addState({
+		id: 'desktop',
+		minWidth: parseInt(_ssm_settings.desktop_min),
+		onEnter: function() 
+		{ 
+			closeMobileMenu();
 			runDesktop();
-        	runTabletOrBigger(); 
-        }
-    });
+			runTabletOrBigger(); 
+		}
+	});
 
-    ssm.ready();
+	ssm.ready();
 }());
 
-
-function initMobileMenu()
-{
-	jQuery(document).ready(function($)
-	{
-		 // mobile nav
-		 var $mainnav = $('#main-nav-container');
-		 	 
-		 $('#mobmenu').on('click', function(e) 
-		 {
-			 e.preventDefault();
-			 $mainnav.toggleClass('open');
-		 });
-	});
-}
-
-/* mobile */
-var mobile_run = false;
-function runMobile()
-{
-	if (!mobile_run)
-	{ 
-		mobile_run = true;
-		initMobileMenu();
-	}
-}
-
-/* tablet */
-var tablet_run = false;
-function runTablet()
-{
-	if (!tablet_run)
-	{ 
-		tablet_run = true;
-		initMobileMenu();
-	}
-}
-
-/* tablet or bigger */
-var tablet_or_bigger_run = false;
-function runTabletOrBigger()
-{
-	if (!tablet_or_bigger_run)
-	{ 
-		tablet_or_bigger_run = true;
-	}	
-}
-
-/* desktop */
-var desktop_run = false;
-function runDesktop()
-{
-	if (!desktop_run)
-	{ 
-		desktop_run = true;
+/* hamburger menu */
+var _bt_menu_run = false;
+function initMobileMenu() {	
+	if (!_bt_menu_run) {
 		
-		jQuery(document).ready(function($)
-		{
-			
+		_bt_menu_run = true;
+		
+		jQuery(document).ready(function($) {			
+			 var $mainnav = $('#main-nav-container');
+			 	 
+			 $('#mobmenu').on('click', function(e) 
+			 {				 
+				 e.preventDefault();
+				 $mainnav.toggleClass('open');
+			 });
 		});
 	}
 }
 
-/* all sizes */
-jQuery(document).ready(function($)
-{
+function closeMobileMenu() {
+	jQuery(document).ready(function($) {
+		$('#main-nav-container').removeClass('open');
+	});
+}
 
-});
+/* mobile */
+var _bt_mobile_run = false;
+function runMobile() {
+	if (!_bt_mobile_run)
+	{ 
+		_bt_mobile_run = true;
+	}
+}
+
+/* tablet */
+var _bt_tablet_run = false;
+function runTablet() {
+	if (!_bt_tablet_run)
+	{ 
+		_bt_tablet_run = true;
+	}
+}
+
+/* tablet or bigger */
+var _bt_tablet_or_bigger_run = false;
+function runTabletOrBigger() {
+	if (!_bt_tablet_or_bigger_run)
+	{ 
+		_bt_tablet_or_bigger_run = true;
+	}	
+}
+
+/* desktop */
+var _bt_desktop_run = false;
+function runDesktop() {
+	if (!_bt_desktop_run)
+	{ 
+		_bt_desktop_run = true;
+		
+		/* jQuery(document).ready(function($) { }); */
+	}
+}
+
+/* all sizes */
+jQuery(document).ready(function($) { });
