101765f0dffe0e03b26ad3543a3d66dc2385140a
Tim Gieseking <timstl@gmail.com>
2017-01-19 15:22:57 -0600

    fix php syntax error in search template

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -13,7 +13,7 @@
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 	</header><!-- .entry-header -->
 
-	<?php the_post_thumbnail()(); ?>
+	<?php the_post_thumbnail(); ?>
 
 	<?php the_excerpt(); ?>
 
