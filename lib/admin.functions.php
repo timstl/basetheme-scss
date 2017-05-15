b5f0911ec41fd4dff60536c1a095074f9fb34fbe
Tim Gieseking <tgieseking@atomicdust.com>
2017-05-15 15:16:31 -0500

    remove admin functions and admin css

--- lib/admin.functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Enqueue an admin-specific css file.
-*/
-function admin_scripts(){
-	wp_enqueue_style( 'btadmin', get_template_directory_uri().'/lib/css/admin.css', array(), cache_bust() );
-}
-add_action( 'wp_enqueue_scripts', 'admin_scripts' );
