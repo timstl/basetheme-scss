28127a6378f00097e7dd4a44ca902bd5eefb291c
Tim Gieseking <tgieseking@atomicdust.com>
2017-06-02 10:54:39 -0500

    re-order meta tags in head so X-UA-Compatible is first.

--- header.php
+++ header.php
@@ -4,13 +4,11 @@
 <!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
-	<?php wp_head(); ?>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <div id="wrapper">
