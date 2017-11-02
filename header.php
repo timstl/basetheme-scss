3f1c22ff5b102f3e627ee971a445fcc2f7556e31
Tim Gieseking <tgieseking@atomicdust.com>
2017-11-02 14:35:13 -0500

    re-add bootstrap 4 scss; update some html for bootstrap 4; update SCSS variables

--- header.php
+++ header.php
@@ -1,12 +1,9 @@
 <!doctype html>
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+<html class="no-js" <?php language_attributes(); ?>>
 <head>
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php wp_head(); ?>
 </head>
@@ -15,39 +12,26 @@
 	<a href="#main" class="sr-only sr-only-focusable skipnav"><?php _e('Skip to main content', 'basetheme'); ?></a>
 	<header id="header" class="clearfix">
 		<div class="container">
-			<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="Home" id="logo"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a>
-			<!-- Begin Navigation -->
-			<nav class="navbar navbar-default" role="navigation">
-				
-				    <!-- Brand and toggle get grouped for better mobile display -->
-				    <div class="navbar-header">
-				      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav-container">
-				        <span class="sr-only">Toggle navigation</span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				      </button>
-				    </div>
-				
-				    <!-- Collect the nav links, forms, and other content for toggling -->
-				    <div class="collapse navbar-collapse" id="main-nav-container">
-				     <?php
-					 	//FB and Twitter are appended in functions.php
-						$args = array(
-							'theme_location' => 'mainnav',
-							'depth'		 => 0,
-							'container'	 => false,
-							'menu_class'	 => 'clearfix',
-							'menu_id' => 'main-nav',
-							'walker'	 => new BootstrapNavMenuWalker()
-						);
-		
-						wp_nav_menu($args);
+			<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="Home" class="logo"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a>
+
+			<nav class="navbar navbar-default">						
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav-container" aria-controls="main-nav-container" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
 			
-					?>
-				    </div><!-- /.navbar-collapse -->
-				    
+				<div class="collapse navbar-collapse" id="main-nav-container">
+					<?php
+					$args = array(
+						'theme_location' => 'mainnav',
+						'depth'		 => 0,
+						'container'	 => false,
+						'menu_class'	 => 'navbar-nav mr-auto',
+						'menu_id' => 'main-nav',
+						'walker'	 => new BootstrapNavMenuWalker()
+					);
+					wp_nav_menu($args);
+				?>
+				</div>
 			</nav>
-			<!-- End Navigation -->
 		</div>
 	</header>
