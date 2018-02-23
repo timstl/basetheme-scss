59e88cc04848b2029b0356189909922237e406a9
Tim Gieseking <tgieseking@atomicdust.com>
2018-02-23 12:47:52 -0600

    update bootstrap 4

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php wp_head(); ?>
