70f6ca4bdee4c34d890832f381017a7d7457fa0b
Tim Gieseking <tgieseking@atomicdust.com>
2018-03-29 13:29:57 -0500

    format gulpfile (no real changes)

--- gulpfile.js
+++ gulpfile.js
@@ -1,119 +1,143 @@
 "use strict";
-var URL = 'http://127.0.0.1:8080/basetheme';
+var URL = "http://127.0.0.1:8080/basetheme";
 
-var $ = require('gulp-load-plugins')(),
-	gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	maps = require('gulp-sourcemaps'),
-	gutil = require('gulp-util'),
-	notify = require('gulp-notify'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	cleanCSS    = require('gulp-clean-css'),
-	squeue = require('streamqueue'),
-	babel = require('gulp-babel'),
-	browserSync = require('browser-sync').create();
+var $ = require("gulp-load-plugins")(),
+	gulp = require("gulp"),
+	sass = require("gulp-sass"),
+	maps = require("gulp-sourcemaps"),
+	gutil = require("gulp-util"),
+	notify = require("gulp-notify"),
+	concat = require("gulp-concat"),
+	uglify = require("gulp-uglify"),
+	cleanCSS = require("gulp-clean-css"),
+	squeue = require("streamqueue"),
+	babel = require("gulp-babel"),
+	browserSync = require("browser-sync").create();
 
 /* For autoprefixer */
-var COMPATIBILITY = [
-	'last 2 versions',
-	'ie >= 10',
-	'Android >= 2.3'
-];
+var COMPATIBILITY = ["last 2 versions", "ie >= 10", "Android >= 2.3"];
 
 function handleErrors() {
 	gutil.beep();
-  var args = Array.prototype.slice.call(arguments);
-  notify.onError({
-	title: 'Compile Error',
-	message: '<%= error.message %>'
-  }).apply(this, args);
-  this.emit('end'); // Keep gulp from hanging on this task
-}	
+	var args = Array.prototype.slice.call(arguments);
+	notify
+		.onError({
+			title: "Compile Error",
+			message: "<%= error.message %>"
+		})
+		.apply(this, args);
+	this.emit("end"); // Keep gulp from hanging on this task
+}
 
 // Browsersync task
-gulp.task('browser-sync', function() {
-
-	var files = [
-						'**/*.php',
-						'img/**/*.{png,jpg,gif}',
-					];
+gulp.task("browser-sync", function() {
+	var files = ["**/*.php", "img/**/*.{png,jpg,gif}"];
 
 	browserSync.init(files, {
 		// Proxy address
-		proxy: URL,
+		proxy: URL
 
 		// Port #
 		//port: PORT
 	});
 });
 
-gulp.task("compileSass", function(){
-	return gulp.src("scss/*.scss")
+gulp.task("compileSass", function() {
+	return gulp
+		.src("scss/*.scss")
 		.pipe(maps.init())
 		.pipe(sass())
-		.on('error', handleErrors)
-		.pipe($.autoprefixer({
-			browsers: COMPATIBILITY
-		}))
+		.on("error", handleErrors)
+		.pipe(
+			$.autoprefixer({
+				browsers: COMPATIBILITY
+			})
+		)
 		.pipe(cleanCSS())
-		.pipe(maps.write('./'))
-		.pipe(gulp.dest('dist'))
+		.pipe(maps.write("./"))
+		.pipe(gulp.dest("dist"))
 		.pipe(browserSync.stream());
 });
 
-gulp.task('lint', function() {
-	return gulp.src('js/footer/custom/*.js')
-		.pipe($.jshint({esversion: 6}))
-		.pipe($.notify(function (file) {
-			if (file.jshint.success) {
-				return false;
-			}
-
-			var errors = file.jshint.results.map(function (data) {
-				if (data.error) {
-					return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
+gulp.task("lint", function() {
+	return gulp
+		.src("js/footer/custom/*.js")
+		.pipe($.jshint({ esversion: 6 }))
+		.pipe(
+			$.notify(function(file) {
+				if (file.jshint.success) {
+					return false;
 				}
-			}).join("\n");
-			return file.relative + " (" + file.jshint.results.length + " errors)\n" + errors;
-		}));
+
+				var errors = file.jshint.results
+					.map(function(data) {
+						if (data.error) {
+							return (
+								"(" +
+								data.error.line +
+								":" +
+								data.error.character +
+								") " +
+								data.error.reason
+							);
+						}
+					})
+					.join("\n");
+				return (
+					file.relative +
+					" (" +
+					file.jshint.results.length +
+					" errors)\n" +
+					errors
+				);
+			})
+		);
 });
 
-gulp.task('scriptshead', function() {
- 	 return squeue(
-				{ objectMode: true },
-				gulp.src(['./js/head/vendor/**/*.js', '!./js/head/vendor/jquery.min.js']).pipe(uglify()),
-				gulp.src(['./js/head/bootstrap/**/*.js']).pipe(uglify()),
-				gulp.src(['./js/head/custom/**/*.js']).pipe(babel({ presets: ['env'] }))
-														.pipe(uglify())
-			)
-			.pipe(concat('head.min.js'))
-			.pipe(gulp.dest('./dist/'))
-			.pipe(browserSync.stream());
+gulp.task("scriptshead", function() {
+	return squeue(
+		{ objectMode: true },
+		gulp
+			.src([
+				"./js/head/vendor/**/*.js",
+				"!./js/head/vendor/jquery.min.js"
+			])
+			.pipe(uglify()),
+		gulp.src(["./js/head/bootstrap/**/*.js"]).pipe(uglify()),
+		gulp
+			.src(["./js/head/custom/**/*.js"])
+			.pipe(babel({ presets: ["env"] }))
+			.pipe(uglify())
+	)
+		.pipe(concat("head.min.js"))
+		.pipe(gulp.dest("./dist/"))
+		.pipe(browserSync.stream());
 });
 
-gulp.task('scriptsfooter', function() {
- 	 return squeue(
-				{ objectMode: true },
-				gulp.src(['./js/footer/vendor/**/*.js']).pipe(uglify()),
-				gulp.src(['./js/footer/bootstrap/**/*.js']).pipe(uglify()),
-				gulp.src(['./js/footer/custom/**/*.js']).pipe(babel({ presets: ['env'] }))
-														.pipe(uglify())
-			)
-			.pipe(concat('scripts.min.js'))
-			.pipe(gulp.dest('./dist/'))
-			.pipe(browserSync.stream());
+gulp.task("scriptsfooter", function() {
+	return squeue(
+		{ objectMode: true },
+		gulp.src(["./js/footer/vendor/**/*.js"]).pipe(uglify()),
+		gulp.src(["./js/footer/bootstrap/**/*.js"]).pipe(uglify()),
+		gulp
+			.src(["./js/footer/custom/**/*.js"])
+			.pipe(babel({ presets: ["env"] }))
+			.pipe(uglify())
+	)
+		.pipe(concat("scripts.min.js"))
+		.pipe(gulp.dest("./dist/"))
+		.pipe(browserSync.stream());
 });
 
 gulp.task("watch", function() {
-	gulp.watch('scss/**/*.scss', ['compileSass']);
-	gulp.watch('js/head/**/*.js', ['scriptshead']);
-	gulp.watch('js/footer/**/*.js', ['scriptsfooter']);
-	gulp.watch('js/head/custom/**/*.js', ['lint']);
-	gulp.watch('js/footer/custom/**/*.js', ['lint']);
+	gulp.watch("scss/**/*.scss", ["compileSass"]);
+	gulp.watch("js/head/**/*.js", ["scriptshead"]);
+	gulp.watch("js/footer/**/*.js", ["scriptsfooter"]);
+	gulp.watch("js/head/custom/**/*.js", ["lint"]);
+	gulp.watch("js/footer/custom/**/*.js", ["lint"]);
 });
 
 gulp.task("default", function() {
-	gulp.start('browser-sync');
-	gulp.start('watch');
+	gulp.start("browser-sync");
+	gulp.start("watch");
 });
