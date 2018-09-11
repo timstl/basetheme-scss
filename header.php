7e45d11239d0688241b4c0d764d045136bd45d8e
Tim Gieseking <tgieseking@atomicdust.com>
2018-09-11 12:41:43 -0500

    begin updating basetheme to comply with WordPress Coding Standards for PHP_CodeSniffer (https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards)

--- header.php
+++ header.php
@@ -1,3 +1,16 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * Displays all of the <head> section, wrapper, logo, and main navigation.
+ *
+ * @package WordPress
+ * @subpackage basetheme
+ * @since 1.0
+ * @version 2.6
+ */
+
+?>
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
@@ -8,26 +21,28 @@
 </head>
 <body <?php body_class(); ?>>
 <div id="wrapper">
-	<a href="#main" class="sr-only sr-only-focusable skipnav"><?php _e('Skip to main content', 'basetheme'); ?></a>
+	<a href="#main" class="sr-only sr-only-focusable skipnav"><?php esc_attr_e( 'Skip to main content', 'basetheme' ); ?></a>
 	<header id="header" class="clearfix">
 		<div class="container">
-			<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="Home" class="logo"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a>
+			<a href="<?php esc_url_e( home_url( '/' ) ); ?>" title="<?php get_bloginfo( 'name', 'display' ); ?>" rel="Home" class="logo"><?php get_bloginfo( 'name', 'display' ); ?></a>
 
 			<nav class="navbar navbar-expand-md navbar-light" role="navigation">
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav-container" aria-controls="main-nav-container" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<?php
-				wp_nav_menu(array(
-					'theme_location' => 'mainnav',
-					'depth' => 2,
-					'container' => 'div',
-					'container_class' => 'collapse navbar-collapse',
-					'container_id' => 'main-nav-container',
-					'menu_class' => 'nav navbar-nav',
-					'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-					'walker' => new WP_Bootstrap_Navwalker(),
-				));
+				wp_nav_menu(
+					array(
+						'theme_location'  => 'mainnav',
+						'depth'           => 2,
+						'container'       => 'div',
+						'container_class' => 'collapse navbar-collapse',
+						'container_id'    => 'main-nav-container',
+						'menu_class'      => 'nav navbar-nav',
+						'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+						'walker'          => new WP_Bootstrap_Navwalker(),
+					)
+				);
 				?>
 			</nav>
 		</div>
