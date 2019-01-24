d037cd228e654815c859e99a146226d2f4b64868
Tim Gieseking <tgieseking@atomicdust.com>
2019-01-24 11:18:16 -0600

    minor block css additions and PHP error fix in comments.php

--- comments.php
+++ comments.php
@@ -65,7 +65,7 @@ if ( post_password_required() ) {
 	// If comments are closed and there are comments, let's leave a little note, shall we?
 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
-		<p class="no-comments"><?php _esc_attr_e( 'Comments are closed.', 'basetheme' ); ?></p>
+		<p class="no-comments"><?php esc_attr_e( 'Comments are closed.', 'basetheme' ); ?></p>
 	<?php endif; ?>
 
 	<?php
