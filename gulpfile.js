f26540cfb99814698747b5155cd2331b8362719f
Tim Gieseking <tgieseking@atomicdust.com>
2019-06-14 14:28:18 -0500

    new gulp setup to prepare for using individual block js/css files (where possible); also includes eslint rules for WordPress Coding Standards

--- gulpfile.js
+++ gulpfile.js
@@ -1,147 +1,152 @@
-"use strict";
-var URL = "http://127.0.0.1:8080/basetheme";
+const proxy_url = '127.0.0.1:8080/basetheme';
 
-var $ = require("gulp-load-plugins")(),
-	gulp = require("gulp"),
-	sass = require("gulp-sass"),
-	maps = require("gulp-sourcemaps"),
-	gutil = require("gulp-util"),
-	notify = require("gulp-notify"),
-	concat = require("gulp-concat"),
-	uglify = require("gulp-uglify"),
-	cleanCSS = require("gulp-clean-css"),
-	squeue = require("streamqueue"),
-	babel = require("gulp-babel"),
-	browserSync = require("browser-sync").create();
+const gulp = require( 'gulp' );
+const sass = require( 'gulp-sass' );
+const postcss = require( 'gulp-postcss' );
+const autoprefixer = require( 'autoprefixer' );
+const cssnano = require( 'cssnano' );
+const sourcemaps = require( 'gulp-sourcemaps' );
+const uglify = require( 'gulp-uglify' );
+const babel = require( 'gulp-babel' );
+const eslint = require( 'gulp-eslint' );
+const concat = require( 'gulp-concat' );
+const mergeStream = require( 'merge-stream' );
+const browserSync = require( 'browser-sync' ).create();
 
-/* For autoprefixer */
-var COMPATIBILITY = ["last 2 versions", "ie >= 10", "Android >= 2.3"];
+const paths = {
+	styles: {
+		src: [ './src/scss/*.scss', '!./src/scss/blocks/standalone/*.scss' ],
+		dest: './dist/css/'
+	},
+	js: {
+		head: {
+			src: {
+				vendor: [
+					'./src/js/head/vendor/**/*.js',
+					'./src/js/head/bootstrap/**/*.js'
+				],
+				custom: [ './src/js/head/custom/**/*.js' ]
+			},
+			dest: './dist/js'
+		},
+		footer: {
+			src: {
+				vendor: [
+					'./src/js/footer/vendor/**/*.js',
+					'./src/js/footer/bootstrap/**/*.js'
+				],
+				custom: [ './src/js/footer/custom/**/*.js' ]
+			},
+			dest: './dist/js'
+		}
+	}
+};
 
-function handleErrors() {
-	gutil.beep();
-	var args = Array.prototype.slice.call(arguments);
-	notify
-		.onError({
-			title: "Compile Error",
-			message: "<%= error.message %>"
-		})
-		.apply(this, args);
-	this.emit("end"); // Keep gulp from hanging on this task
+function style() {
+	return (
+		gulp
+			.src( paths.styles.src )
+
+			// Initialize sourcemaps before compilation starts
+			.pipe( sourcemaps.init() )
+			.pipe( sass() )
+			.on( 'error', sass.logError )
+
+			// Use postcss with autoprefixer and compress the compiled file using cssnano
+			.pipe( postcss([ autoprefixer(), cssnano() ]) )
+
+			// Now add/write the sourcemaps
+			.pipe( sourcemaps.write( './' ) )
+			.pipe( gulp.dest( paths.styles.dest ) )
+
+			// Add browsersync stream pipe after compilation
+			.pipe( browserSync.stream() )
+	);
 }
 
-function bsync() {
-	var files = ["**/*.php", "img/**/*.{png,jpg,gif}"];
+function doJS( scripts_nobabel, scripts_babel, scripts_dest, filename ) {
+	var streams = [];
 
-	browserSync.init(files, {
-		// Proxy address
-		proxy: URL
-
-		// Port #
-		//port: PORT
+	scripts_nobabel.forEach( function( path ) {
+		streams.push( gulp.src( path ).pipe( uglify() ) );
 	});
-}
 
-function compileSass() {
-	return gulp
-		.src(["src/scss/*.scss"])
-		.pipe(maps.init())
-		.pipe(sass())
-		.on("error", handleErrors)
-		.pipe(
-			$.autoprefixer({
-				browsers: COMPATIBILITY
-			})
-		)
-		.pipe(cleanCSS())
-		.pipe(maps.write("./"))
-		.pipe(gulp.dest("./dist/css/"))
-		.pipe(browserSync.stream());
-}
-
-function lintjs() {
-	return gulp
-		.src("js/footer/custom/*.js")
-		.pipe($.jshint({ esversion: 6 }))
-		.pipe(
-			$.notify(function(file) {
-				if (file.jshint.success) {
-					return false;
-				}
-
-				var errors = file.jshint.results
-					.map(function(data) {
-						if (data.error) {
-							return (
-								"(" +
-								data.error.line +
-								":" +
-								data.error.character +
-								") " +
-								data.error.reason
-							);
-						}
-					})
-					.join("\n");
-				return (
-					file.relative +
-					" (" +
-					file.jshint.results.length +
-					" errors)\n" +
-					errors
-				);
-			})
+	scripts_babel.forEach( function( path ) {
+		streams.push(
+			gulp
+				.src( path )
+				.pipe( eslint() )
+				.pipe( eslint.format() )
+				.pipe( eslint.failAfterError() )
+				.pipe( babel({ presets: [ '@babel/preset-env' ] }) )
+				.pipe( uglify() )
 		);
+	});
+
+	return mergeStream( streams )
+		.pipe( concat( filename ) )
+		.pipe( gulp.dest( scripts_dest ) )
+		.pipe( browserSync.stream() );
 }
 
 function scriptshead() {
-	return squeue(
-		{ objectMode: true },
-		gulp.src(["./src/js/head/vendor/**/*.js"]).pipe(uglify()),
-		gulp.src(["./src/js/head/bootstrap/**/*.js"]).pipe(uglify()),
-		gulp
-			.src(["./src/js/head/custom/**/*.js"])
-			.pipe(babel({ presets: ["@babel/preset-env"] }))
-			.pipe(uglify())
-	)
-		.pipe(concat("head.min.js"))
-		.pipe(gulp.dest("./dist/js/"))
-		.pipe(browserSync.stream());
+	return doJS(
+		paths.js.head.src.vendor,
+		paths.js.head.src.custom,
+		paths.js.head.dest,
+		'head.min.js'
+	);
 }
 
 function scriptsfooter() {
-	return squeue(
-		{ objectMode: true },
-		gulp.src(["./src/js/footer/vendor/**/*.js"]).pipe(uglify()),
-		gulp.src(["./src/js/footer/bootstrap/**/*.js"]).pipe(uglify()),
-		gulp
-			.src(["./src/js/footer/custom/**/*.js"])
-			.pipe(babel({ presets: ["@babel/preset-env"] }))
-			.pipe(uglify())
-	)
-		.pipe(concat("scripts.min.js"))
-		.pipe(gulp.dest("./dist/js/"))
-		.pipe(browserSync.stream());
+	return doJS(
+		paths.js.footer.src.vendor,
+		paths.js.footer.src.custom,
+		paths.js.footer.dest,
+		'scripts.min.js'
+	);
 }
 
-function scriptsblocks() {
-	return squeue(
-		{ objectMode: true },
-		gulp
-			.src(["./src/js/blocks/**/*.js"])
-			.pipe(babel({ presets: ["@babel/preset-env"] }))
-			.pipe(uglify())
-	)
-		.pipe(gulp.dest("./dist/js/"))
-		.pipe(browserSync.stream());
+// Add browsersync initialization at the start of the watch task
+function watch() {
+	browserSync.init([ '**/*.php', 'img/**/*.{png,jpg,gif}' ], {
+
+		// If you are already serving your website locally using something like apache
+		// You can use the proxy setting to proxy that instead
+		proxy: proxy_url
+	});
+
+	gulp.watch( paths.styles.src, style );
+	gulp.watch( paths.js.head.src.vendor, scriptshead );
+	gulp.watch( paths.js.head.src.custom, scriptshead );
+	gulp.watch( paths.js.footer.src.vendor, scriptsfooter );
+	gulp.watch( paths.js.footer.src.custom, scriptsfooter );
 }
 
-function watchFiles() {
-	gulp.watch(["src/scss/**/*.scss"], compileSass);
-	gulp.watch("src/js/blocks/**/*.js", scriptsblocks);
-	gulp.watch("src/js/head/**/*.js", scriptshead);
-	gulp.watch("src/js/footer/**/*.js", scriptsfooter);
-	gulp.watch("src/js/head/custom/**/*.js", lintjs);
-	gulp.watch("src/js/footer/custom/**/*.js", lintjs);
-}
+// We don't have to expose the reload function
+// It's currently only useful in other functions
 
-exports.default = gulp.parallel(watchFiles, bsync);
+// Don't forget to expose the task!
+exports.watch = watch;
+
+// Expose the task by exporting it
+// This allows you to run it from the commandline using
+// $ gulp style
+exports.style = style;
+exports.scriptshead = scriptshead;
+exports.scriptsfooter = scriptsfooter;
+
+/*
+ * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
+ */
+var build = gulp.parallel( watch );
+
+/*
+ * You can still use `gulp.task` to expose tasks
+ */
+//gulp.task('build', build);
+
+/*
+ * Define default task that can be called by just running `gulp` from cli
+ */
+gulp.task( 'default', build );
