a23d339a85d080623511f91f32951a2715e0041e
Tim Gieseking <tgieseking@atomicdust.com>
2019-06-19 11:14:14 -0500

    more formatting improvements

--- lib/theme-shortcodes.php
+++ lib/theme-shortcodes.php
@@ -10,54 +10,58 @@
  * @version 2.7
  */
 
-/**
- *  Button shortcode
- *
- * @param array $atts Attributes passed into shortcode function.
- */
-function bt_bootstrap_button_shortcode( $atts ) {
-	$a = shortcode_atts(
-		array(
-			'class'  => '',
-			'href'   => '',
-			'text'   => '',
-			'target' => '',
-			'link'   => '',
-		),
-		$atts
-	);
+if ( ! function_exists( 'bt_bootstrap_button_shortcode' ) ) {
+	/**
+	 *  Button shortcode
+	 *
+	 * @param array $atts Attributes passed into shortcode function.
+	 */
+	function bt_bootstrap_button_shortcode( $atts ) {
+		$a = shortcode_atts(
+			array(
+				'class'  => '',
+				'href'   => '',
+				'text'   => '',
+				'target' => '',
+				'link'   => '',
+			),
+			$atts
+		);
 
-	$target = '';
+		$target = '';
 
-	if ( '' !== $a['class'] ) {
-		$a['class'] = ' ' . $a['class'];
-	} else {
-		$a['class'] = ' btn-primary';
+		if ( '' !== $a['class'] ) {
+			$a['class'] = ' ' . $a['class'];
+		} else {
+			$a['class'] = ' btn-primary';
+		}
+
+		if ( '' === $a['href'] && '' !== $a['link'] ) {
+			$a['href'] = $a['link'];
+		}
+
+		if ( '' !== $a['target'] ) {
+			$target = ' target="' . strip_tags( $a['target'] ) . '"';
+		}
+
+		return '<a href="' . esc_url( $a['href'] ) . '" class="btn' . strip_tags( $a['class'] ) . '"' . $target . '><span>' . esc_attr( $a['text'] ) . '</span></a>';
 	}
-
-	if ( '' === $a['href'] && '' !== $a['link'] ) {
-		$a['href'] = $a['link'];
-	}
-
-	if ( '' !== $a['target'] ) {
-		$target = ' target="' . strip_tags( $a['target'] ) . '"';
-	}
-
-	return '<a href="' . esc_url( $a['href'] ) . '" class="btn' . strip_tags( $a['class'] ) . '"' . $target . '><span>' . esc_attr( $a['text'] ) . '</span></a>';
 }
 add_shortcode( 'button', 'bt_bootstrap_button_shortcode' );
 
-/**
- * Button group shortcode.
- *
- * @param array  $atts Attributes passed into shortcode function.
- * @param string $content Content passed into shortcode (buttons).
- */
-function bt_bootstrap_buttongroup_shortcode( $atts = [], $content = null ) {
-	// Remove HTML from buttons. This kills all BRs, but there shouldn't be HTML in your buttons anyway.
-	// Could modify later if we find we want to add SVG support within buttons.
-	$content = strip_tags( $content );
-	$content = do_shortcode( $content );
-	return '<div class="btn-group" role="group" aria-label="button group">' . $content . '</div>';
+if ( ! function_exists( 'bt_bootstrap_buttongroup_shortcode' ) ) {
+	/**
+	 * Button group shortcode.
+	 *
+	 * @param array  $atts Attributes passed into shortcode function.
+	 * @param string $content Content passed into shortcode (buttons).
+	 */
+	function bt_bootstrap_buttongroup_shortcode( $atts = [], $content = null ) {
+		// Remove HTML from buttons. This kills all BRs, but there shouldn't be HTML in your buttons anyway.
+		// Could modify later if we find we want to add SVG support within buttons.
+		$content = strip_tags( $content );
+		$content = do_shortcode( $content );
+		return '<div class="btn-group" role="group" aria-label="button group">' . $content . '</div>';
+	}
 }
 add_shortcode( 'buttongroup', 'bt_bootstrap_buttongroup_shortcode' );
