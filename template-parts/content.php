e2d3a3ecdb8ce7e5e7b17c0a7dc231755576614f
Tim Gieseking <tgieseking@atomicdust.com>
2019-06-20 10:05:59 -0500

    additional improvements to content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -13,23 +13,33 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		if ( is_single() ) {
+		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
-		} else {
-			the_title( '<h2 class="entry-title">', '</h2>' );
-		}
+		else :
+			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		endif;
 		?>
 	</header><!-- .entry-header -->
 
-	<?php the_post_thumbnail(); ?>
+	<?php
+	if ( has_post_thumbnail() ) :
+		if ( is_single() ) :
+			the_post_thumbnail();
+		else :
+			?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail(); ?></a>
+			<?php
+		endif;
+	endif;
+	?>
 
 	<section class="entry">
 		<?php
-		if ( is_single() ) {
+		if ( is_single() ) :
 			the_content();
-		} else {
+		else :
 			the_excerpt();
-		}
+		endif;
 		?>
 	</section><!-- .entry-content -->
 </article><!-- #post-## -->
