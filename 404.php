7f0310661f06d43af31eb9af7f4d24b940f1fa6e
Tim Gieseking <tgieseking@atomicdust.com>
2019-06-26 13:19:58 -0500

    remove unnecessary admin messages in a couple files + fix translation string on search

--- 404.php
+++ 404.php
@@ -17,11 +17,7 @@ get_header(); ?>
 		</div>
 	</header>
 	<div class="container">
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'basetheme' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
-		<?php elseif ( is_search() ) : ?>
+		<?php if ( is_search() ) : ?>
 
 			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'basetheme' ); ?></p>
 			<?php get_search_form(); ?>
--- search.php
+++ search.php
@@ -16,7 +16,7 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'basetheme' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h1>
+				<h1 class="page-title"><?php _e( 'Search results:' ); ?> <?php printf( '%s', '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<?php
