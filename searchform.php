4898bd1d88a7c8a8414431609d48c2cccc822609
Tim Gieseking <tgieseking@atomicdust.com>
2019-06-26 14:06:22 -0500

    fix search form escaping/echo issues

--- searchform.php
+++ searchform.php
@@ -10,12 +10,17 @@
 
 ?>
 
-<?php $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
+<?php
+/**
+ * In case there is more than 1 search form on a page.
+ */
+$unique_id = esc_attr( uniqid( 'search-form-' ) );
+?>
 
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label for="<?php esc_attr_e( $unique_id ); ?>">
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'basetheme' ); ?></span>
+	<label for="<?php echo $unique_id; ?>">
+		<span class="screen-reader-text"><?php _e( 'Search for:', 'basetheme' ); ?></span>
 	</label>
-	<input type="search" id="<?php esc_attr_e( $unique_id ); ?>" class="search-field" placeholder="<?php esc_attr_e_x( 'Search &hellip;', 'placeholder', 'basetheme' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-	<button type="submit" class="search-submit"><?php echo _x( 'Search', 'submit button', 'basetheme' ); ?></button>
+	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php _e( 'Search &hellip;', 'basetheme' ); ?>" value="<?php esc_attr_e( get_search_query() ); ?>" name="s" />
+	<button type="submit" class="search-submit"><?php _e( 'Search', 'basetheme' ); ?></button>
 </form>
