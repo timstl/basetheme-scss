bad24ecb5c0053b7050ee630f4cf206863799e40
Tim Gieseking <tgieseking@atomicdust.com>
2020-02-06 15:52:02 -0600

    add color/size formats to basetheme

--- lib/theme-functions.php
+++ lib/theme-functions.php
@@ -90,3 +90,74 @@ if ( ! function_exists( 'bt_editor_add_theme_support' ) ) {
 	}
 }
 add_action( 'init', 'bt_editor_add_theme_support', 3 );
+
+/**
+ * Modify Tiny MCE Settings.
+ * This is for ACF blocks that use the content editor.
+ *
+ * This is similar to what is done in the "Basetheme Modify Core Blocks" plugin, but is more likely to be modified in each theme.
+ */
+add_filter( 'mce_buttons_2', 'bt_theme_add_mce_buttons' );
+add_filter( 'tiny_mce_before_init', 'bt_theme_modify_tiny_mce_style_formats' );
+
+if ( ! function_exists( 'bt_theme_add_mce_buttons' ) ) {
+	function bt_theme_add_mce_buttons( $buttons ) {
+		if ( ! in_array( 'styleselect', $buttons ) ) {
+			array_unshift( $buttons, 'styleselect' );
+		}
+		return $buttons;
+	}
+}
+
+if ( ! function_exists( 'bt_theme_modify_tiny_mce_style_formats' ) ) {
+
+	function bt_theme_modify_tiny_mce_style_formats( $settings ) {
+
+		$style_formats = array();
+		if ( isset( $settings['style_formats'] ) && $settings['style_formats'] != '' ) {
+			if ( ! is_array( $settings['style_formats'] ) ) {
+				$settings['style_formats'] = json_decode( $settings['style_formats'] );
+			}
+			$style_formats = $settings['style_formats'];
+		}
+
+		foreach ( array(
+			'Primary'   => 'Primary',
+			'Secondary' => 'Secondary',
+			'Success'   => 'Success',
+			'Info'      => 'Info',
+			'Warning'   => 'Warning',
+			'Danger'    => 'Danger',
+		) as $key => $color ) {
+			$style_formats[] = array(
+				'title'    => 'Color: ' . $color . ' (' . $key . ')',
+				'classes'  => 'color--' . strtolower( $key ),
+				'selector' => 'p,h1,h2,h3,h4,h5,h6,li',
+				'inline'   => false,
+			);
+		}
+
+		foreach ( array( 'Small', 'Normal', 'Medium', 'Large', 'Huge' ) as $size ) {
+			$style_formats[] = array(
+				'title'    => 'Size: ' . $size,
+				'classes'  => 'has-' . strtolower( $size ) . '-font-size',
+				'selector' => 'p,h1,h2,h3,h4,h5,h6,li',
+				'inline'   => false,
+			);
+		}
+
+		/*
+		Example of adding is-style-serif to the editor. You can then add this class to _utilities.scss.
+
+		$style_formats[] = array(
+		'title'    => 'Serif',
+		'classes'  => 'is-style-serif',
+		'selector' => 'p,h1,h2,h3,h4,h5,h6,li',
+		'inline'   => false,
+		);
+		*/
+		$settings['style_formats'] = json_encode( $style_formats );
+
+		return $settings;
+	}
+}
