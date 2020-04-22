625d379b0c75aeea32a55a75df3e33941db8976a
Tim Gieseking <tgieseking@atomicdust.com>
2020-04-22 11:06:52 -0500

    increase re-usability of pageheader block by adding a full width photo + content version.

--- template-parts/blocks/content-pageheader.php
+++ template-parts/blocks/content-pageheader.php
@@ -8,23 +8,8 @@
 
 // Create id attribute for specific styling.
 $block_id = 'pageheader-' . $block['id'];
-
-$classes = array( 'pageheader' );
-
-/**
- * Substitute Bootstrap classes for alignwide/aligncenter.
- * Page headers should probably always be alignfull.
- */
-$align_class = $block['align'] ? 'align' . $block['align'] : '';
-if ( ! $align_class ) {
-	$align_class = 'alignfull';
-} elseif ( $align_class == 'alignwide' ) {
-	$align_class = 'container-wide';
-} elseif ( $align_class == 'aligncenter' ) {
-	$align_class = 'container';
-}
-$classes[] = $align_class;
-
+$classes  = array( 'pageheader' );
+$styles   = array();
 
 /**
  * Custom classes added in admin.
@@ -37,17 +22,12 @@ if ( have_rows( 'pageheader' ) ) :
 	while ( have_rows( 'pageheader' ) ) :
 		the_row();
 
-		$layout = get_row_layout();
-
+		$layout    = get_row_layout();
 		$classes[] = 'pageheader--' . $layout;
 
 		if ( get_sub_field( 'background_dim' ) && get_sub_field( 'background_dim' ) > 0 ) {
 			$classes[] = 'has-background-dim';
 			$classes[] = 'has-background-dim--' . intval( get_sub_field( 'background_dim' ) );
-		}
-
-		if ( get_sub_field( 'background_fixed' ) ) {
-			$classes[] = 'pageheader--fixed';
 		}
 
 		if ( is_admin() ) {
@@ -58,40 +38,52 @@ if ( have_rows( 'pageheader' ) ) :
 			$classes[] = ' inview';
 		}
 
+		/**
+		 * Does this block have a background image set?
+		 */
+		$image = get_sub_field( 'background_image' );
+		if ( $image && isset( $image['url'] ) ) {
+			$styles[] = 'background-image:url(' . esc_url( $image['url'] ) . ');';
+
+
+			$position = get_sub_field( 'background_position' );
+			if ( $position ) {
+				/**
+				 * ACF issue where we have seen both an array or string returned for this value.
+				 * Possibly a bug in 5.8-beta that was fixed at some point?
+				 */
+				if ( is_array( $position ) ) {
+					$position = $position[0];
+				}
+
+				$styles[] = 'background-position:' . wp_strip_all_tags( $position ) . ';';
+			}
+		}
 		?>
 		<header id="<?php echo $block_id; ?>" class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
-			<?php if ( $layout == 'image_content' ) : ?>
-			<div class="pageheader-text">
-				<?php the_sub_field( 'content' ); ?>
-			</div>
-				<?php
-				if ( get_sub_field( 'image' ) ) :
-					if ( get_sub_field( 'background_position' ) ) {
-						$position = get_sub_field( 'background_position' );
-						/**
-						 * ACF issue where we have seen both an array or string returned for this value.
-						 * Possibly a bug in 5.8-beta that was fixed at some point?
-						 */
-						if ( is_array( $position ) ) {
-							$position = $position[0];
-						}
-					} else {
-						$position = 'center center';
-					}
-					?>
-				<style type="text/css">
-					#<?php echo $block_id; ?> {
-						background-position: <?php echo wp_strip_all_tags( $position ); ?>;
-						background-image: url(<?php echo esc_url( get_sub_field( 'image' )['url'] ); ?>);
-					}
-				</style>
-				<?php endif; ?>
-			<?php endif; ?>
+			<?php
+			/**
+			 * Get this layout's template part.
+			 */
+			get_template_part( 'template-parts/blocks/pageheader/layout', $layout );
+			?>
 		</header>
 		<?php
-endwhile;
-elseif ( is_admin() ) :
-	?>
+		/**
+		 * Output this block's styles, if we have any.
+		 */
+		if ( ! empty( $styles ) ) :
+			?>
+		<style type="text/css">
+			#<?php echo $block_id; ?> {
+				<?php echo implode( '', $styles ); ?>
+			}
+		</style>
+			<?php
+		endif;
+	endwhile;
+	elseif ( is_admin() ) :
+		?>
 <p><em><?php esc_attr_e( 'Please add a header layout.', 'basetheme' ); ?></em></p>
-	<?php
+		<?php
 endif; ?>
---
+++ template-parts/blocks/pageheader/layout-full_width_photo.php
@@ -0,0 +1,7 @@
+<div class="container">
+	<div class="row">
+		<div class="col col-24 col-md-15 col-xl-12 pageheader__content">
+			<?php the_sub_field( 'content' ); ?>
+		</div>
+	</div>
+</div>
